129d2f90845b7dc9bc8b858dbf81adbea1eff34f
deemoniak <corentin.grandchamps@gmail.com>
2020-03-20 17:34:21 +0100

    Mini TTP°2 get file from request

--- MiniTP2/my-app/script.js
+++ MiniTP2/my-app/script.js
@@ -2,36 +2,52 @@ const fs = require('fs');
 const http = require('http');
 const pug = require('pug');
 
-
-const filename = process.argv[2];
 const compiledFunction = pug.compileFile('template.pug');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    let url = req.url;
+    let filename = "";
+
+    if(url.indexOf("filename") !== -1) {
+        filename = url.split("filename=")[1];
+        if(filename.indexOf('&') !== -1) {
+            filename = filename.split('&')[0];
+        }
     
-    fs.readFile('./' + filename,'utf8', (err, data) => {
-        if(err) {
-            console.log(err);
-            return
-        } 
-        let jsonData = {"infos" : {}};
-        let allCSVligns = data.split('\n');
-        let count = 0;
-        allCSVligns.forEach( (element) => {
-            const user = element.split(';')[0];
-            const city = element.split(';')[1];
-            const newInfo = 'info' + count;
-            jsonData.infos[newInfo] = {"user" : user, "city": city};
-            count++; 
-        })
-        
-        const generatedTemplate = compiledFunction({
-            allInfos: jsonData.infos
-        })
-        res.statusCode = 200;
-        res.setHeader("Content-Type","text/html");
-        res.end(generatedTemplate);
-    })
+        if(filename !== "" && filename !== null && filename !== undefined) { 
+            fs.readFile('./' + filename,'utf8', (err, data) => {
+                if(err) {
+                    console.log(err);
+                    return
+                } 
+                let jsonData = {"infos" : {}};
+                let allCSVligns = data.split('\n');
+                let count = 0;
+                allCSVligns.forEach( (element) => {
+                    const user = element.split(';')[0];
+                    const city = element.split(';')[1];
+                    const newInfo = 'info' + count;
+                    jsonData.infos[newInfo] = {"user" : user, "city": city};
+                    count++; 
+                })
+                
+                const generatedTemplate = compiledFunction({
+                    allInfos: jsonData.infos
+                })
+                res.statusCode = 200;
+                res.setHeader("Content-Type","text/html");
+                res.end(generatedTemplate);
+            })
+        } else {
+            const generatedTemplate = compiledFunction({
+                error: "Indiquez dans l'url le nom d'un fichier comme ceci : filename=data.csv"
+            })
+            res.statusCode = 200;
+            res.setHeader("Content-Type","text/html");
+            res.end(generatedTemplate);
+        }
+    }
 })
 
 server.listen(port, () => {
