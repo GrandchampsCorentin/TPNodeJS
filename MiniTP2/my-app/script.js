eae598f8376eebc2de54db897d25a41ea1236964
deemoniak <corentin.grandchamps@gmail.com>
2020-03-20 17:57:33 +0100

    Mini TP°2 Code factorisation

--- MiniTP2/my-app/script.js
+++ MiniTP2/my-app/script.js
@@ -18,50 +18,53 @@ const server = http.createServer((req, res) => {
         if(filename !== "" && filename !== null && filename !== undefined) { 
             fs.readFile('./' + filename,'utf8', (err, data) => {
                 if(err) {
-                    const generatedTemplate = compiledFunction({
-                        error: "Fichier introuvable"
-                    })
-                    res.statusCode = 200;
-                    res.setHeader("Content-Type","text/html");
-                    res.end(generatedTemplate);
+                    sendHTML(res, 'error', "Fichier introuvable");
                     return
                 } 
-                let jsonData = {"infos" : {}};
-                let allCSVligns = data.split('\n');
-                let count = 0;
-                allCSVligns.forEach( (element) => {
-                    const user = element.split(';')[0];
-                    const city = element.split(';')[1];
-                    const newInfo = 'info' + count;
-                    jsonData.infos[newInfo] = {"user" : user, "city": city};
-                    count++; 
-                })
-                
-                const generatedTemplate = compiledFunction({
-                    allInfos: jsonData.infos
-                })
-                res.statusCode = 200;
-                res.setHeader("Content-Type","text/html");
-                res.end(generatedTemplate);
+                const jsonData = csvToJson(data);
+                sendHTML(res, 'allInfos', jsonData.infos);
             })
         } else {
-            const generatedTemplate = compiledFunction({
-                error: "Fichier introuvable"
-            })
-            res.statusCode = 200;
-            res.setHeader("Content-Type","text/html");
-            res.end(generatedTemplate);
+            sendHTML(res, 'error', "Fichier introuvable");
         }
     } else {
-        const generatedTemplate = compiledFunction({
-            error: "Indiquez dans l'url le nom d'un fichier comme ceci : filename=data.csv"
-        })
-        res.statusCode = 200;
-        res.setHeader("Content-Type","text/html");
-        res.end(generatedTemplate);
+        sendHTML(res, 'error', "Indiquez dans l'url le nom d'un fichier comme ceci : filename=data.csv")
     }
 })
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`);
 })
+
+
+function csvToJson(data) {
+
+    let jsonData = {"infos" : {}};
+    let allCSVligns = data.split('\n');
+    let count = 0;
+    allCSVligns.forEach( (element) => {
+        const user = element.split(';')[0];
+        const city = element.split(';')[1];
+        const newInfo = 'info' + count;
+        jsonData.infos[newInfo] = {"user" : user, "city": city};
+        count++; 
+    })
+
+    return jsonData;
+}
+
+function sendHTML(res, payloadName, payload) {
+    let generatedTemplate;
+    if(payloadName === "error") {
+        generatedTemplate = compiledFunction({
+            error: payload
+        }) 
+    } else {
+        generatedTemplate = compiledFunction({
+            allInfos: payload
+        })
+    }
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.end(generatedTemplate);
+}
